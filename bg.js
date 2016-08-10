c1ba48d8f6c4dbce60f8d778e89b8e02ba27cc5a
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-08-10 21:35:02 +0900

    remove MSEdge blocker + event page

--- bg.js
+++ bg.js
@@ -1,18 +1,27 @@
-var imageOnClick = (info, tab) => {
-  chrome.storage.local.get('autoSearch', (res) => {
+if (!self.browser && self.chrome) {
+  browser = chrome;
+}
+
+browser.runtime.onInstalled.addListener(() => {
+  browser.contextMenus.create({
+    id: 'search-on-whatanime.ga',
+    title: 'Search on whatanime.ga',
+    contexts: ['image']
+  })
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId !== "search-on-whatanime.ga") {
+    return;
+  }
+
+  browser.storage.local.get('autoSearch', (res) => {
     let searchURL = 'https://whatanime.ga/?auto&url=' + info.srcUrl
     if (res.autoSearch === false) {
       searchURL = 'https://whatanime.ga/?url=' + info.srcUrl
     }
-    chrome.tabs.create({
+    browser.tabs.create({
       url: searchURL
     })
   })
-}
-
-chrome.contextMenus.create({
-  id: 'search-on-whatanime.ga',
-  title: 'Search on whatanime.ga',
-  contexts: ['image'],
-  onclick: imageOnClick
-})
+});
